ebf9e22123d248505746a65cdd87d0e1bcc1eff1
Savin Angel-Mario <hello@m4.rocks>
2025-08-25 12:40:46 +0300

    Test build commit

--- drizzle.remote.config.ts
+++ drizzle.remote.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
 	dialect: "sqlite",
 	driver: "d1-http",
 	dbCredentials: {
-		accountId: process.env.CF_ACCOUNT_ID || "",
-		databaseId: process.env.CF_DATABASE_ID || "",
-		token: process.env.CF_DATABASE_TOKEN || "",
+		accountId: process.env.CLOUDFLARE_ACCOUNT_ID || "",
+		databaseId: process.env.CLOUDFLARE_DATABASE_ID || "",
+		token: process.env.CLOUDFLARE_ACCOUNT_TOKEN || "",
 	},
 });
